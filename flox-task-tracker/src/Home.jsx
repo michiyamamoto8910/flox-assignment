e0aa0c630b11b6b85b73dee77ce3e2214ecb10ae
myamamoto4 <myamamoto4@dxc.com>
2025-03-20 21:57:29 +0800

    Initial setup for project

---
+++ flox-task-tracker/src/Home.jsx
@@ -0,0 +1,12 @@
+import './home.css'
+const Home = () => {
+    return (
+        <>
+        <div class="header">
+            <h1 class="header-title">Flox</h1> 
+        </div>
+        </>
+    )
+}
+
+export default Home;
---
+++ flox-task-tracker/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Home from './Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>
+);
