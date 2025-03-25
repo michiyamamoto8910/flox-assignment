fbbb17ba4202ead77010f7493b6227405ec5b503
michiyamamoto8910 <michihiroyamamoto123@gmail.com>
2025-03-25 21:05:47 +0800

    remove unused css for normal bootstrap

--- flox-task-tracker/src/index.js
+++ flox-task-tracker/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Home from './Home';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
